3130ee8c0fc08414865fb7cf972a15a4b1c8fb5e
Murmus <skolor@gmail.com>
2016-09-17 11:55:33 -0400

    now not having two copies...

--- events/csaw2016/tutorial/patch.py
+++ events/csaw2016/tutorial/patch.py
@@ -3,9 +3,11 @@ a = f.read()
 f.close()
 
 offset = a.index("\xE8\x00\xF9\xFF\xFF")
+assert a.count("\xE8\x00\xF9\xFF\xFF")==1
 noalarm = a[:offset]+"\x90"*5+a[offset+5:]
 
-offset=a.index("\xE8\x39\xFB\xFF\xFF")
+offset=a.index("\xE8\x39\xFB\xFF\xFF\x89\x45\xcc")
+assert a.count("\xE8\x39\xFB\xFF\xFF\x89\x45\xcc") == 1
 nopriv = noalarm[:offset]+"\x90"*5+noalarm[offset+5:]
 
 open("tutorial_",'w').write(nopriv)
