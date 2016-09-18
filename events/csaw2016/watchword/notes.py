871b6f5fe4176f0f4859ad33c0c8685580eb3586
Murmus <skolor@gmail.com>
2016-09-18 12:16:27 -0400

    watchword solve

---
+++ events/csaw2016/watchword/notes.py
@@ -0,0 +1,11 @@
+Used binwalk to find offset for a png
+
+Ripped that out manually
+
+used stepic to pull a jpeg out of the png
+
+used steghide with password=password to get out text
+
+python3:
+
+base64.b85decode(base64.txt)
